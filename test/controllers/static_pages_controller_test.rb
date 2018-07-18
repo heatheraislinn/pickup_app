032b6691044cb9b71a76132657e35305167fa0ec
Heather Shar <heatheraislinn@gmail.com>
2018-07-18 18:37:31 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,28 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Pickup"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    assert_select "title", "About | Pickup"
   end
 
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | Pickup"
   end
 
 end
